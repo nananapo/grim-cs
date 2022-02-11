83eaaf7598ce84ea692063d9d22fac3b7c35b7cc
na <kanapodream@gmail.com>
2022-02-11 19:33:21 +0900

    Update

--- Grim/VM/FunctionCall.cs
+++ Grim/VM/FunctionCall.cs
@@ -2,18 +2,19 @@ namespace Grim.VM;
 
 public class FunctionCall : IFormula
 {
-    public readonly string Name;
+
+    public readonly IFormula Lambda;
 
     public readonly List<IFormula> Parameters;
 
-    public FunctionCall(string name,List<IFormula>? parameters = null)
+    public FunctionCall(IFormula lambda,List<IFormula> parameters)
     {
-        Name = name;
-        Parameters = parameters ?? new ();
+        Lambda = lambda;
+        Parameters = parameters;
     }
 
     public override string ToString()
     {
-        return nameof(FunctionCall) + $"<{Name}><{string.Join(",",Parameters.Select(v=>v.ToString()))}>";
+        return nameof(FunctionCall) + $"<{Lambda}><{string.Join(",",Parameters.Select(v=>v.ToString()))}>";
     }
 }
